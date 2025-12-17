506f794f3cc55a10f3d632dc679385fa9aada345
bala-amypo <bala@amypo.in>
2025-12-17 06:42:06 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -3,4 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.dom.entity.Student
+import com.example.dom.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Interger>{
+    
+}
--- src/main/java/com/example/demo/service/studentserviceimpl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/studentserviceimpl/StudentServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.entity.Student;
-import com.example.demo.repostiory.StudentRepostiory;
+import com.example.demo.repository.StudentRepository;
 import com.example.demo.service.StudentService;
 
 import org.springframework.stereotype.Service;
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Service;
 public class StudentServiceImpl{
 
     @Autowired
-    StudentRepostiory repo;
+    StudentRepository repo;
 
 
     @Override
