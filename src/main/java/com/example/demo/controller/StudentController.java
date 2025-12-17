5e0ca9659a0a6b17f337fd77e58e9aea5d27c732
bala-amypo <bala@amypo.in>
2025-12-17 09:35:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -25,5 +25,9 @@ public class StudentController {
     public List<Student> fetchRecord(){
         return ser.fetchRecord();
     }
+    @GetMapping("/fetchdatabyid/{id}")
+    public Optional<Student> fetchDataById(@PathVariable int id){
+        return ser.fetchDataById
+    }
 
 }
